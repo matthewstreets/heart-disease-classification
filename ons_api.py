1919e1c412164659a0bdf51a156fe82273dc961c
Matthew Streets <87211678+matthewstreets@users.noreply.github.com>
2023-03-09 13:21:15 +0000

    removed ons.json and ons_api.py

--- ons_api.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-
-reply = requests.get('https://api.beta.ons.gov.uk/v1/datasets')
-
-data = reply.json()
-
-with open('ons.json', 'w') as file:
-    json.dump(data, file)
